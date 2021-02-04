117e0ceb1f2bcabc1d40fc0b35f173eac0b524dc
Amir <am8896626>
2021-02-05 03:22:08 +0330

    shooting and enemy parts complited

--- main.cpp
+++ main.cpp
@@ -16,19 +16,23 @@ public:
 	int hp;
 	int hpmax ;
 	Sprite  Shape ;
+	float speedx =7;
+	float speedy =0;
+
 	Enemy(Texture *texture , Vector2u windowsize )
 	{
 		this-> hpmax = rand()%3 + 1;
 		this->hp = this->hpmax ;
 		this->Shape.setTexture(*texture);
 		this->Shape.setScale(0.1f, 0.1f);
-		this->Shape.setPosition(rand()%windowsize.x - this->Shape.getGlobalBounds().width, this->Shape.getGlobalBounds().height);
+		this->Shape.setPosition(rand()%windowsize.x - this->Shape.getGlobalBounds().width, rand()%200 -this->Shape.getGlobalBounds().height);
 
 	}
 
 	~Enemy(){}
 
 };
+
 
 class Bullet
 {
@@ -49,8 +53,7 @@ int main()
 {
 	srand(time(NULL));
 
-	RenderWindow window(sf::VideoMode(1000, 800), "space invades",  sf::Style::Titlebar | sf::Style::Close
-                         | sf::Style::Resize );
+	RenderWindow window(sf::VideoMode(1000, 800), "space invades",  sf::Style::Titlebar | sf::Style::Close | sf::Style::Resize );
     window.setMouseCursorVisible(true);
     window.setFramerateLimit(60);
 	//Player
@@ -59,6 +62,7 @@ int main()
 	player.setPointCount(3);
 	player.setOrigin(50.f, 50.f);
     player.setPosition(950, 750) ;
+	int Hp =12 ;
 
     Texture ship ;
     ship.loadFromFile("ship.png");
@@ -67,29 +71,27 @@ int main()
 	//Bullets
 	Bullet b1;
 	std::vector<Bullet> bullets;
-
 	
-
 	//Vectors
 	Vector2f playerCenter;
 	Vector2f mousePosWindow;
 	Vector2f aimDir;
 	Vector2f aimDirNorm;
 
-	//Enemy
-	/*RectangleShape enemy;
-	enemy.setFillColor(Color::White);
-	enemy.setPosition(100 , 100) ;
-	enemy.setSize(Vector2f(50.f, 50.f));
-	int spawnCounter = 20;
-	
-	int y ;*/
-	Vector2f movement(5.f ,0.f );
+
+	//enemy	
 	Texture ent ;
 	ent.loadFromFile("enemyve.png");
-
 	std::vector<Enemy> enemies;
-	enemies.push_back(Enemy(&ent , window.getSize()));
+	int enemyspowntimer1 = 0 , enemyspowntimer2 ;
+
+
+
+	//limit line
+	RectangleShape line(Vector2f(window.getSize().x, 10.f));
+	line.setFillColor(Color::Red);
+	line.setPosition(Vector2f(0.f, 550.f));
+
 
 	while (window.isOpen())
 	{
@@ -118,20 +120,39 @@ int main()
 			player.move(-10.f, 0.f);
 		if (Keyboard::isKeyPressed(Keyboard::D))
 			player.move(10.f, 0.f);
+		if (Keyboard::isKeyPressed(Keyboard::W))
+			player.move(0.f, -10.f);
+		if (Keyboard::isKeyPressed(Keyboard::S))
+			player.move(0.f, 10.f);
 			
 			//dont go out :))
 		
 		if(player.getPosition().x <= 0)
-		{
-			player.setPosition(0.f, player.getPosition().y) ;
+		{	
+			player.setPosition(window.getSize().x , player.getPosition().y) ;
+			
 		}
 		else if(player.getPosition().x >= window.getSize().x)
 		{
 			//player.setPosition(window.getSize().x - player.getLocalBounds().width, player.getPosition().y) ;
-			player.setPosition(window.getSize().x , player.getPosition().y) ;
+			player.setPosition(0.f, player.getPosition().y) ;
+		}
+		else if (player.getPosition().y >= window.getSize().y)
+		{
+			player.setPosition(player.getPosition().x, window.getSize().y -10);
+		}
+		else if (player.getPosition().y <= 600)
+		{
+			player.setPosition(player.getPosition().x, 610.f);
 		}
 		
-		
+			
+		window.clear();
+		window.draw(player);
+		for (size_t i = 0; i < bullets.size(); i++)		
+			window.draw(bullets[i].shape);
+
+
 		//Shooting
 		if (Mouse::isButtonPressed(Mouse::Left))
 		{
@@ -148,60 +169,89 @@ int main()
 			//Out of bounds
 			if (bullets[i].shape.getPosition().x < 0 || bullets[i].shape.getPosition().x > window.getSize().x
 				|| bullets[i].shape.getPosition().y < 0 || bullets[i].shape.getPosition().y > window.getSize().y)
-				bullets.erase(bullets.begin() + i);
-
-			
+				{
+					bullets.erase(bullets.begin() + i);
+					break ;
+					
+				}
 				
+
+			//colision
+			else
+			{
+				//Enemy collision
+				for (size_t k = 0; k < enemies.size(); k++)
+				{
+					if (bullets[i].shape.getGlobalBounds().intersects(enemies[k].Shape.getGlobalBounds()))
+					{
+						bullets.erase(bullets.begin() + i);
+						enemies.erase(enemies.begin() + k);
+						break;
+					}
+				}
+			}				
 		}
+
+		
+
+
 
 		//enemy 
+			//spown
+		if(enemyspowntimer1 < 100)
+		++enemyspowntimer1 ;
 
-		//enemy movment control
-		/*if(enemy.getPosition().x >= window.getSize().x -25)
+
+
+		if(enemyspowntimer1 >= 100)
 		{
-			movement.x *= -1;
-
-			enemy.setPosition(enemy.getPosition().x, enemy.getPosition().y+50 ) ;
+			enemies.push_back(Enemy(&ent , window.getSize())); 
+			enemyspowntimer1 = 0 ;
 		}
-		if(enemy.getPosition().x <= 0)
-		{
-			movement.x *= -1;
-			enemy.setPosition(enemy.getPosition().x, enemy.getPosition().y+50 );
-		}
-	
-		enemy.move(movement) ;*/
-
-		//Draw
-		window.clear();
-		window.draw(player);
-
-		for (size_t i = 0; i < bullets.size(); i++)		
-			window.draw(bullets[i].shape);
-		//window.draw(enemy);
+			//moving
 		for (size_t i = 0; i < enemies.size(); i++)
 		{	
-			//enemy move
-			if(enemies[i].Shape.getPosition().x >= window.getSize().x -25)
+			
+			if(enemies[i].Shape.getPosition().x >= window.getSize().x -50)
 			{
-				movement.x *= -1;
-
-				enemies[i].Shape.setPosition(enemies[i].Shape.getPosition().x, enemies[i].Shape.getPosition().y+50 ) ;
+				enemies[i].speedx *= -1.2;
+				//every time it will be faster than befor
+				enemies[i].Shape.setPosition(enemies[i].Shape.getPosition().x, enemies[i].Shape.getPosition().y+100 ) ;
 			}
 			if(enemies[i].Shape.getPosition().x <= 0)
 			{
-				movement.x *= -1;
-				enemies[i].Shape.setPosition(enemies[i].Shape.getPosition().x, enemies[i].Shape.getPosition().y+50 );
+				enemies[i].speedx *= -1.2;
+				enemies[i].Shape.setPosition(enemies[i].Shape.getPosition().x, enemies[i].Shape.getPosition().y+100 );
 			}
 	
-			enemies[i].Shape.move(movement) ;
+			enemies[i].Shape.move(Vector2f(enemies[i].speedx, enemies[i].speedy)) ;
 
+			if (enemies[i].Shape.getPosition().y<= -enemies[i].Shape.getGlobalBounds().height )
+			{
+				enemies.erase(enemies.begin()+i);
+				break ;
+			}
+
+			/*if(enemies[i].Shape.getPosition().y>=550)
+			{
+				enemies.erase(enemies.begin()+i);
+				--Hp;
+				break;
+				
+			}*/
+		
+			if (enemies[i].Shape.getGlobalBounds().intersects(player.getGlobalBounds()))
+			{
+				enemies.erase(enemies.begin()+i);
+				Hp -=5;
+				break;
+			}
+									
 			window.draw(enemies[i].Shape);
 
-
 		}
-		
 
-		
+		window.draw(line);
 		window.display();
 	}
 	return 0;
